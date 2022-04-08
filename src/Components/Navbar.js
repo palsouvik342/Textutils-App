70c351ea94d3059160af0d4697ecad77bd761aaa
palsouvik342 <99487285+palsouvik342@users.noreply.github.com>
2022-04-09 01:41:17 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = (props) => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-               {/*   <a className="nav-link active" aria-current="page" href="#">Home</a> */}
+               <a className="nav-link active" aria-current="page" href="#">Home</a>
                {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
                 </li>
                 <li className="nav-item">
