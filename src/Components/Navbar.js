f4bd57e97cde22fcf7a4e553b2a5585ed632009b
palsouvik342 <99487285+palsouvik342@users.noreply.github.com>
2022-04-09 02:07:44 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = (props) => {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode==='white'?'#000033':'#fff'}`}>
         <div className="container-fluid">
-            <Link className="navbar-brand" to="/">{props.title}</Link>
+            <a className="navbar-brand" href="/">{props.title}</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
